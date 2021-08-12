0c5e6ae6f6144b7404be240ef510e8efd7673cde
Settawat Boriruklert <z.z.z.z.z.z.z.zenith@gmail.com>
2021-08-12 20:16:21 +0700

    finished setup sensor for v2

--- Client/Project492_Hardware/src/SetupV2/Setup.h
+++ Client/Project492_Hardware/src/SetupV2/Setup.h
@@ -2,6 +2,9 @@
 #include <Wire.h>
 #include "RPR-0521RS.h"
 
+#define TCA_ADDR_0  0x70
+#define TCA_ADDR_1  0x71
+
 void TCA9548A(uint8_t bus,int address)
 {
   Wire.beginTransmission(address);
@@ -9,14 +12,27 @@ void TCA9548A(uint8_t bus,int address)
   Wire.endTransmission();
 }
 
-void SetupSensor(RPR0521RS rpr[],int device_max)
+template <size_t row,size_t col>
+void SetupSensor(RPR0521RS (&rpr)[row][col],int (*tcaAddr))
 {
-    byte rc[device_max];
-    for(int i=0;i<device_max;i++)
+    byte rc[row][col];
+    int addrNum=0;
+    int num = 0;
+    for(int i=0;i<row;i++)
     {
-        TCA9548A(i,0x70);
+      for(int j=0;j<rol;j++)
+      {
+        TCA9548A(num,tcaAddr[addrNum]);
         while (!Serial);
-        rc[i] = rpr[i].init();
-        Serial.println(rc[i]);
+        rc[i][j] = rpr[i][j].init();
+        Serial.println(rc[i][j]);
+        num++;
+
+      }
+      if(num == 8)
+      {
+        num = 0;
+        addrNum++;
+      }
     }
 }
