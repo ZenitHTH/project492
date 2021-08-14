bd7961f46ca1eda3f85c136f9298cfde68a14292
Settawat Boriruklert <z.z.z.z.z.z.z.zenith@gmail.com>
2021-08-14 19:51:20 +0700

    run at 2d sensor

--- Client/Project492_Hardware/src/main.cpp
+++ Client/Project492_Hardware/src/main.cpp
@@ -2,23 +2,45 @@
 #include <Wire.h>
 #include "RPR-0521RS.h"
 #include "SerialMonitor.h"
-#include "Setup.h"
+#include "SetupV2/Setup.h"
+#include "SetupV2/Data.h"
+#include "SetupV2/Board.h"
 
-#define DEVICE_MAX 6
-RPR0521RS rpr0521rs[DEVICE_MAX];
 
- 
-void setup() {
-  // put your setup code here, to run once:
+
+#define DEVICE_COL  4
+#define DEVICE_ROW  4 
+RPR0521RS rpr0521rs[DEVICE_ROW][DEVICE_COL];
+int tcaAddr[] = {0x70,0x71};
+//RPR0521RS rpr0521rs[DEVICE_MAX];
+
+void setup()
+{
   Serial.begin(115200);
   Wire.begin();
-  SetupSensor(rpr0521rs,DEVICE_MAX);
+  SetupSensor(rpr0521rs,tcaAddr);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  Board board();
+  GetValue(rpr0521rs,tcaAddr,&board());
 
-  Data dat = GetValue(rpr0521rs,DEVICE_MAX);
-  PrintSersorStatus(dat.GetPS(),dat.GetAls(),DEVICE_MAX);
-  delay(5000);
+  board().PrintStatus();
 }
+
+ 
+// void setup() {
+//   // put your setup code here, to run once:
+//   Serial.begin(115200);
+//   Wire.begin();
+//   SetupSensor(rpr0521rs,DEVICE_MAX);
+// }
+
+// void loop() {
+//   // put your main code here, to run repeatedly:
+
+//   Data dat = GetValue(rpr0521rs,DEVICE_MAX);
+//   PrintSersorStatus(dat.GetPS(),dat.GetAls(),DEVICE_MAX);
+//   delay(5000);
+// }
