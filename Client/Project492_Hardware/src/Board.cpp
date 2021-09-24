fd40cd255182b6163b7dc9b8717b1c9daff96202
ZenitHTH <z.z.z.z.z.z.z.zenith@gmail.com>
2021-09-25 04:17:17 +0700

    fixed a fatal bug

--- Client/Project492_Hardware/src/Board.cpp
+++ Client/Project492_Hardware/src/Board.cpp
@@ -1,7 +1,7 @@
 #include "Board.h"
 
 
-Board::Board(Data **data,int row,int col)
+Board::Board(Data **data,uint8_t row,uint8_t col)
 {
     this->len_x = row;
     this->len_y = col;
@@ -12,7 +12,6 @@ Board::Board()
 {
     this->len_x = 0;
     this->len_y = 0;
-    
 }
 
 Board Board::operator=(const Board& b)
@@ -29,9 +28,9 @@ Board Board::operator=(const Board& b)
 bool Board::HaveData()
 {
 
-    for(int i=0;i< this->len_x;i++)
+    for(uint8_t i=0;i< this->len_x;i++)
     {
-        for(int j=0;j<this->len_y;j++)
+        for(uint8_t j=0;j<this->len_y;j++)
         {
             if(this->data[i][j].HaveItData() == false) return false;
         }
@@ -44,16 +43,16 @@ bool Board::HaveData()
 bool** Board::PinDetect()
 {
     bool **pinStatus;
-    pinStatus = new bool* [len_x];
+    pinStatus = new bool* [this->len_x];
     if(this->HaveData())
     {
-        for(int i=0;i<this->len_x;i++)
+        for(uint8_t i=0;i<this->len_x;i++)
         {
-            pinStatus[i] = new bool [len_y];
-            for(int j=0;j<this->len_y;j++)
+            pinStatus[i] = new bool [this->len_y];
+            for(uint8_t j=0;j<this->len_y;j++)
             {
                 if(data[i][j].GetAls() < 10.00) pinStatus[i][j] = 1;
-                else pinStatus = 0;
+                else pinStatus[i][j] = 0;
             }
         }
     }
@@ -64,9 +63,9 @@ bool** Board::PinDetect()
 void Board::PrintStatus()
 {
     Serial.println();
-    for(int i=0;i<len_x;i++)
+    for(uint8_t i=0;i<len_x;i++)
     {
-        for(int j=0;j<len_y;j++)
+        for(uint8_t j=0;j<len_y;j++)
         {
             Serial.print("["+String(data[i][j].GetAls(),2)+"]");
             Serial.print("["+String(data[i][j].GetPS(),DEC)+"]\t");
